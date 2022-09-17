367d12edcb26cb295d273dcb0f88b6b3e0abfd99
Nahid-07 <nahidasif1998@gmail.com>
2022-09-17 12:37:29 +0600

    footer daynamic cart and daynamic modal added

---
+++ js/script.js
@@ -0,0 +1,58 @@
+const cartContainer = document.querySelector('#cart-container')
+const modalContainer = document.getElementById('modal-container')
+const phoneData = async()=>{
+    const res = await fetch('../data.json');
+    const data = await res.json();
+    return data
+}
+// phoneData()
+ 
+const phoneDetils =async ()=>{
+    const phones =await phoneData()
+    // console.log(phones);
+    phones.forEach(element => {
+        const {img, name,price,id} = element
+        const div = document.createElement('div');
+        div.classList.add("card", "bg-base-100", "shadow-lg")
+        // console.log(element);
+        div.innerHTML =`
+        <figure><img src="${img}" alt="Shoes" /></figure>
+        <div class="card-body">
+          <div class="flex justify-between">
+            <h2 class="card-title">${name}</h2>
+            <p><span class="font-medium text-xl">Price :</span> <span class="font-semibold text-lg">$799</span></p>
+            <div>
+              <i class="fa-solid fa-heart mr-4 text-red-700"></i>
+              <i class="fa-solid fa-square-minus  text-red-700"></i>
+            </div>
+          </div>
+          <p><span class="font-semibold text-xl">Price :</span> <span class="font-semibold text-xl">$${price}</span></p>
+          <div class="card-actions justify-between">
+            <button class="btn btn-outline btn-primary">Buy now</button>
+            <label onclick="modalDetails('${id}')" for="my-modal-3" class="btn btn-outline btn-secondary">See details</label>
+          </div>
+        </div>
+        `;
+        cartContainer.appendChild(div)
+    })
+}
+phoneDetils()
+
+const modalDetails =async (id) =>{
+    const phones = await phoneData();
+    const product = phones.find(element => element.id === id)
+    console.log(product)
+    const {img, name, price} = product;
+    modalContainer.innerHTML =` 
+    <div class="modal-box relative">
+    <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+    <div class="p-10">
+    <img src="${img}" alt="">
+    </div>
+    <h2 class="card-title">${name}</h2>
+    <p><span class="font-medium text-xl">Price :</span> <span class="font-semibold text-lg">$ ${price}</span></p>
+  </div>
+    `
+
+}
+// modalDetails ()
